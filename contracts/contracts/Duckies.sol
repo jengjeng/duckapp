47a774cf090efc62c9c59e477066dfd82421cb1b
MoD <mod@tail.cat>
2022-05-16 02:30:16 +0300

    Adding Duckies contract

---
+++ contracts/contracts/Duckies.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/// @custom:security-contact security@ducks.house
+contract YellowDuckies is Initializable, ERC20CappedUpgradeable, PausableUpgradeable, OwnableUpgradeable {
+
+    // Maximum Supply
+    uint256 private constant _MAX_SUPPLY = 888000000000000;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("Yellow Duckies", "DUCKZ");
+        __ERC20Capped_init(_MAX_SUPPLY * 10 ** decimals());
+        __Pausable_init();
+        __Ownable_init();
+
+        _mint(msg.sender, 44400000000000 * 10 ** decimals());
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     */
+    function burn(uint256 amount) public onlyOwner {
+        _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public onlyOwner {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
